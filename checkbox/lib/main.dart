7715360f8b7e45d9b750eef5422412792734624b
ADMIN <surendhiran734@gmail.com>
2023-11-17 11:28:01 +0530

    final - commit

---
+++ checkbox/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:checkbox/checkbox_listview.dart';
+import 'package:checkbox/checkbox_listview_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      //home: CheckBoxListview(),
+       home: CheckboxListviewScreen(),
+    );
+  }
+}
